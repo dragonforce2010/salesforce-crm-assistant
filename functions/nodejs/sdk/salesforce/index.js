b20d80f7c4597cbd194398e4e0bcbcd0729e4317
Michael Zhang <michael.zhang@bytedance.com>
2024-09-10 22:17:54 +0800

    milestone1: 完成项目脚手架和salesforceSdk mvp

--- functions/nodejs/sdk/salesforce/index.js
+++ functions/nodejs/sdk/salesforce/index.js
@@ -1,16 +1,25 @@
 const { Client } = require('./client')
 const jsforce = require('jsforce')
 
-const salesforceClient = async() => {
+const salesforceClient = async(logger, context) => {
   const conn = new jsforce.Connection({
     oauth2: {
-      clientId: '',
-      clientSecret: '',
-      loginUrl: ''
+      clientId: '3MVG9k02hQhyUgQDLqXDVQVzvg.ns2L3OfrYnfLtQNbOlzJSpVGMoDUUSCduVVRELL2RC4f8lBM8BpIs27Lb1',
+      clientSecret: 'B55AE749CA7F9DEB0471046EAB128F22AE5BB47AC5C8E687C56A81B634742772',
+      loginUrl: 'https://login.salesforce.com'
     },
-    instanceUrl: ''
+    instanceUrl: 'https://personalaction-dev-ed.develop.my.salesforce.com',
+    refreshFn: async (conn, callback) => {
+      try{
+        const userInfo = await conn.login('michael.zhang@apaas.com', 'apaastraining920')
+        logger.info(`token refreshed using user account ${userInfo}`)
+        callback(null, conn.accessToken)
+      } catch(error) {
+        logger.error('error occurred! error = ', error)
+      }
+    }
   })
-  const client = new Client(null)
+  const client = new Client(conn)
   return client
 }
 
