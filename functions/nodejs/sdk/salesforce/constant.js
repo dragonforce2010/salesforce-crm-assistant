8ca9e899c061b40a1e3fa8005ebe9032bf1873cf
Michael Zhang <michael.zhang@bytedance.com>
2024-09-11 14:48:59 +0800

    milestone4: 使用环境变量进行配置,移除hardcode配置,同时优化常量字符串

---
+++ functions/nodejs/sdk/salesforce/constant.js
@@ -0,0 +1,17 @@
+const TOKEN_REDIS_KEY_PREFIX = 'sf.accessToken';
+const LOGIN_URL = 'salesforceLoginUrl';
+const CLIENT_ID = 'salesforceClientId';
+const CLIENT_SECRET = 'salesforceClientSecret';
+const SALESFORCE_USERNAME = 'salesforceUserName';
+const SALESFORCE_PASSWORD = 'salesforcePassword';
+const SALESFORCE_INSTANCE_URL = 'salesforceInstanceUrl';
+
+module.exports = {
+  TOKEN_REDIS_KEY_PREFIX,
+  LOGIN_URL,
+  CLIENT_ID,
+  CLIENT_SECRET,
+  SALESFORCE_USERNAME,
+  SALESFORCE_PASSWORD,
+  SALESFORCE_INSTANCE_URL,
+};
--- functions/nodejs/sdk/salesforce/index.js
+++ functions/nodejs/sdk/salesforce/index.js
@@ -1,19 +1,28 @@
 const { Client } = require('./client')
 const jsforce = require('jsforce')
+const { LOGIN_URL, SALESFORCE_INSTANCE_URL, CLIENT_ID, CLIENT_SECRET, SALESFORCE_USERNAME, SALESFORCE_PASSWORD, TOKEN_REDIS_KEY_PREFIX } = require('./constant')
 
 const salesforceClient = async(context, logger) => {
-  const token = await getTokenFromRedis(context, logger)
+  const accessToken = await getTokenFromRedis(context, logger)
+  const {
+    loginUrl,
+    instanceUrl,
+    clientId,
+    clientSecret,
+    userName,
+    password
+  } = await getSalesforceConfig()
   const conn = new jsforce.Connection({
     oauth2: {
-      clientId: '3MVG9k02hQhyUgQDLqXDVQVzvg.ns2L3OfrYnfLtQNbOlzJSpVGMoDUUSCduVVRELL2RC4f8lBM8BpIs27Lb1',
-      clientSecret: 'B55AE749CA7F9DEB0471046EAB128F22AE5BB47AC5C8E687C56A81B634742772',
-      loginUrl: 'https://login.salesforce.com'
+      clientId,
+      clientSecret,
+      loginUrl,
     },
-    instanceUrl: 'https://personalaction-dev-ed.develop.my.salesforce.com',
-    accessToken: token,
+    instanceUrl,
+    accessToken,
     refreshFn: async (conn, callback) => {
       try{
-        const userInfo = await conn.login('michael.zhang@apaas.com', 'apaastraining920')
+        const userInfo = await conn.login(userName, password)
         logger.info(`token refreshed using user account ${JSON.stringify(userInfo)}`)
         callback(null, conn.accessToken)
         await saveTokenToRedis(context, logger, conn.accessToken)
@@ -27,12 +36,30 @@ const salesforceClient = async(context, logger) => {
 }
 
 const getTokenFromRedis = async (context, logger) => {
-  const tokenData = await baas.redis.get(`sfToken_${context.user._id}`)
+  const tokenData = await baas.redis.get(`${TOKEN_REDIS_KEY_PREFIX}_${context.user._id}`)
   return tokenData
 }
 
 const saveTokenToRedis = async (context, logger, token) => {
-  await baas.redis.set(`sfToken_${context.user._id}`, token)
+  await baas.redis.set(`${TOKEN_REDIS_KEY_PREFIX}_${context.user._id}`, token)
+}
+
+const getSalesforceConfig = async() => {
+  const loginUrl = await application.globalVar.getVar(LOGIN_URL)
+  const instanceUrl = await application.globalVar.getVar(SALESFORCE_INSTANCE_URL)
+  const clientId = await application.globalVar.getVar(CLIENT_ID)
+  const clientSecret = await application.globalVar.getVar(CLIENT_SECRET)
+  const userName = await application.globalVar.getVar(SALESFORCE_USERNAME)
+  const password = await application.globalVar.getVar(SALESFORCE_PASSWORD)
+
+  return {
+    loginUrl,
+    instanceUrl,
+    clientId,
+    clientSecret,
+    userName,
+    password
+  }
 }
 
 module.exports = {
